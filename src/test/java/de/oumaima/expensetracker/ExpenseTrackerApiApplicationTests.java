6acda4fec11744ef6a49bf1592114cdfc6a02fea
Oumaima Ben El Bey <101521347+Oumaima-Ben-EL-Bey@users.noreply.github.com>
2026-06-07 16:54:02 +0200

    ci: add GitHub Actions workflow to build and test on every push (#1)

--- src/test/java/de/oumaima/expensetracker/ExpenseTrackerApiApplicationTests.java
+++ src/test/java/de/oumaima/expensetracker/ExpenseTrackerApiApplicationTests.java
@@ -2,9 +2,19 @@ package de.oumaima.expensetracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ExpenseTrackerApiApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new
+			PostgreSQLContainer<>("postgres:16");
 
 	@Test
 	void contextLoads() {
